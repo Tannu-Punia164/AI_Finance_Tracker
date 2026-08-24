e9afb6246d60d443513347808b847e37dbb84f42
officialtannu100@gmail.com <officialtannu100@gmail.com>
2026-08-24 12:41:17 +0530

    new changes

--- components/hero.jsx
+++ components/hero.jsx
@@ -56,7 +56,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="/dashboard">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
